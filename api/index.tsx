c4ab8cb4d28625d426f8c9b9077d6447f6e5c89f
Eldar Faaiz <iniakunbisnisku@gmail.com>
2024-06-17 11:36:34 +0700

    update

--- api/index.tsx
+++ api/index.tsx
@@ -28,7 +28,7 @@ app.frame('/', async c => {
   return c.res({
     title: appTitle,
     image: (
-      <Box grow alignVertical="center" background-Image="./public/bg.png" padding="0" >
+      <Box grow alignVertical="center" backgroundColor="white" padding="32" border={BORDER_SIMPLE}>
         <VStack gap="4">
           <Heading color="h1Text" align="center" size="64">
             Quiz time!
